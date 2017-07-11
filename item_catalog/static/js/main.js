4e6bec32b63b4ac8326940186743addf45edd307
Junior Báez <juniorbaez01@gmail.com>
2017-07-11 00:32:58 -0400

    Update Bulma, add main.js

--- item_catalog/static/js/main.js
+++ item_catalog/static/js/main.js
@@ -1,5 +1,11 @@
-// Delete notifications
-console.log("Removing notification.");
+// Delete notifications support
 $('.notifications button.delete').click(function(){
 	$(this).parent().remove();
+});
+
+// Hamburguer menu support
+$('.navbar .navbar-burger').click(
+function(){
+	$(this).toggleClass('is-active');
+	$('.navbar .navbar-menu').toggleClass('is-active');
 });
